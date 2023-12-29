77b215b65b6ec214ed42663102c8b3f4ea7f36e9
Brian Kiragu <bkariuki@hotmail.com>
2023-12-29 15:51:42 +0300

    feat: Added WiFi support

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <Arduino.h>
 #include <TrafficLights.h>
+#include <WiFi.h>
 
 const uint8_t RED_PIN = 15;
 const uint8_t GREEN_PIN = 2;
 const uint8_t BLUE_PIN = 4;
 
+const char *ssid = "Faiba";
+const char *password = "kariuki@2022";
+
+WiFiServer server(80);
+
 void setup()
 {
+    // Serial output.
+    Serial.begin(115200);
+
     // Built-in LED pin.
     pinMode(LED_BUILTIN, OUTPUT);
 
@@ -14,6 +23,31 @@ void setup()
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
+
+    // Delay for pins to set.
+    delay(10);
+
+    // Setup complete text.
+    Serial.println("Mjolnir starting...");
+    Serial.println();
+
+    Serial.print("Mjolnir connecting to: ");
+    Serial.print(ssid);
+    Serial.println();
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+
+    Serial.println("WiFi connected.");
+    Serial.print("IP address: ");
+    Serial.print(WiFi.localIP());
+    Serial.println();
+    server.begin();
 }
 
 void loop()
